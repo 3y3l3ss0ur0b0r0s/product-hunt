a9c679892153e70df57f1b8981b6263f324f53c1
3y3l3ss0ur0b0r0s <danielle.bertulfo.coding@gmail.com>
2025-01-06 21:33:50 -0500

    Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'product-hunt';
+  products: Map<string, any>[] = []
+
+  ngOnInit() {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(json => {
+        json.forEach((product: any) => {
+          let newProduct = new Map([
+            ['title', product['title']],
+            ['upvotes', 0],
+            ['downvotes', 0]
+          ]);
+          this.products.push(newProduct);
+        });
+      });
+  }
 }
